3ba1ecf1f9e0ab39b5a2f5752a490034003ab06e
Corey Clark <coreychristianclark@gmail.com>
2023-05-01 17:15:51 -0400

    Got a head start on writing down the variables and functions

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
+const bold = document.getElementById(".bold");
+const italicize = document.getElementById(".italicize");
+const underline = document.getElementById(".underline");
+const grow = document.getElementById(".grow");
+const shrink = document.getElementById(".shrink");
+const invert = document.getElementById(".invert");
+const red = document.getElementById(".red");
+const blue = document.getElementById(".blue");
+const green = document.getElementById(".green");
+const blur = document.getElementById(".blur");
+const clear = document.getElementById(".clear")
 
+function bold() {}
+
+function italicize() {}
+
+function underline() {}
+
+function grow() { }
+
+function shrink() { }
+
+function invert() { }
+
+function red() { }
+
+function blue() { }
+
+function green() { }
+
+function blur() { }
+
+function clear() {}
