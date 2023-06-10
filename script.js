ec0dad1c6a75870448108312de7545afcdbe2fc0
Corey Clark <coreychristianclark@gmail.com>
2023-06-10 11:18:41 -0400

    Merely adding event listeners and giving the buttons effects one at a time

--- script.js
+++ script.js
@@ -1,41 +1,54 @@
-const italicize = document.getElementById(".italicize");
-const underline = document.getElementById(".underline");
-const grow = document.getElementById(".grow");
-const shrink = document.getElementById(".shrink");
-const invert = document.getElementById(".invert");
-const red = document.getElementById(".red");
-const blue = document.getElementById(".blue");
-const green = document.getElementById(".green");
-const blur = document.getElementById(".blur");
-const clear = document.getElementById(".clear");
+const p = document.querySelector("p");
+const bold = document.querySelector(".bold");
+const italicize = document.querySelector(".italicize");
+const underline = document.querySelector(".underline");
+const grow = document.querySelector(".grow");
+const shrink = document.querySelector(".shrink");
+const invert = document.querySelector(".invert");
+const red = document.querySelector(".red");
+const blue = document.querySelector(".blue");
+const green = document.querySelector(".green");
+const blur = document.querySelector(".blur");
+const clear = document.querySelector(".clear");
+
+// function underline() {}
+
+// function grow() {}
+
+// function shrink() {}
+
+// function invert() {}
+
+// function red() {}
+
+// function blue() {}
+
+// function green() {}
+
+// function blur() {}
+
+// function clear() {}
+
+bold.addEventListener("click", () => {
+  p.style.fontWeight = "bold";
+});
+
+italicize.addEventListener("click", () => {
+  p.style.fontStyle = "italic";
+});
+
+underline.addEventListener("click", () => {
+  p.style.textDecoration = "underline";
+});
 
 
-function bold() {
-    p.style.fontWeight = "bold";
-}
+const currentFontSize = 25;
+const currentClicks = 0
 
-function italicize() {}
+grow.addEventListener("click", () => {
+    if (currentClicks < 3) {
+        currentFontSize += 1
+        p.style.fontSize = currentFontSize + "px"
+    }
 
-function underline() {}
-
-function grow() { }
-
-function shrink() { }
-
-function invert() { }
-
-function red() { }
-
-function blue() { }
-
-function green() { }
-
-function blur() { }
-
-function clear() { }
-
-
-let p = document.getElementById("p");
-const bold = document.getElementById("bold");
-
-bold.addEventListener("click", bold);
+})
