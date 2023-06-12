333d7572bf5eb9f7099fd6145c3e558c70f97c51
Corey Clark <coreychristianclark@gmail.com>
2023-06-11 22:56:13 -0400

    Got the buttons pretty much where I want them to be at. I need to get the 'clear' button up and going, and then I need to work on some cosmetics with this project

--- script.js
+++ script.js
@@ -12,15 +12,18 @@ const blurEffect = document.querySelector(".blur");
 const clear = document.querySelector(".clear");
 
 bold.addEventListener("click", () => {
-  p.style.fontWeight = "bold";
+  p.classList.toggle("boldEffect");
+  bold.classList.toggle("buttonDown");
 });
 
 italicize.addEventListener("click", () => {
-  p.style.fontStyle = "italic";
+  p.classList.toggle("italic");
+  italicize.classList.toggle("buttonDown");
 });
 
 underline.addEventListener("click", () => {
-  p.style.textDecoration = "underline";
+  p.classList.toggle("underlineEffect");
+  underline.classList.toggle("buttonDown");
 });
 
 let currentFontSize = 25;
@@ -31,6 +34,14 @@ grow.addEventListener("click", () => {
     clickCounter++;
     currentFontSize += 1;
     p.style.fontSize = currentFontSize + "px";
+  } else if (clickCounter === 3) {
+    grow.classList.remove("buttonDown");
+    clickCounter = 0;
+    currentFontSize = 25;
+    p.style.fontSize = "";
+  }
+  if (clickCounter === 1) {
+    grow.classList.add("buttonDown");
   }
 });
 
@@ -39,23 +50,35 @@ shrink.addEventListener("click", () => {
     clickCounter++;
     currentFontSize -= 1;
     p.style.fontSize = currentFontSize + "px";
+  } else if (clickCounter === 3) {
+    shrink.classList.remove("buttonDown");
+    clickCounter = 0;
+    currentFontSize = 25;
+    p.style.fontSize = "";
+  }
+  if (clickCounter === 1) {
+    shrink.classList.add("buttonDown");
   }
 });
 
 invert.addEventListener("click", () => {
   p.classList.toggle("upsideDown");
+  invert.classList.toggle("buttonDown");
 });
 
 red.addEventListener("click", () => {
   p.classList.toggle("danger");
+  red.classList.toggle("buttonDown");
 });
 
 blue.addEventListener("click", () => {
   p.classList.toggle("aqua");
+  blue.classList.toggle("buttonDown");
 });
 
 green.addEventListener("click", () => {
   p.classList.toggle("go");
+  green.classList.toggle("buttonDown");
 });
 
 let blurFilter = 0;
@@ -66,13 +89,19 @@ blurEffect.addEventListener("click", () => {
   if (clickCounter < 4) {
     blurFilter += 1;
     p.style.filter = "blur(" + blurFilter + "px)";
-  } else {
-    blurFilter = 0;
-    p.style.filter = "none";
+  } else if (clickCounter === 4) {
+    blurEffect.classList.remove("buttonDown");
     clickCounter = 0;
+    p.style = "";
+    blurFilter = 0;
+  }
+  if (clickCounter === 1) {
+    blurEffect.classList.add("buttonDown");
   }
 });
 
-clear.addEventListener('click', () => {
-    p.style = ""
-})
+clear.addEventListener("click", () => {
+  p.style = "";
+});
+
+JUST HAVE TO GET THE CLEAR FUNCTION WORKING.
